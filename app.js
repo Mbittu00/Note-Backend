92114912d8cb049decee71c02af6e29703439fd7
mbittu00 <mbittu000@gmail.com>
2021-12-13 13:10:37 +0530

    problam fixd

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const mongo=require('./db')
 const app=express()
+let port=prosess.env.PORT || 4001
 app.use(express.json())
 mongo()
 app.use('/set',require('./route/set'))
