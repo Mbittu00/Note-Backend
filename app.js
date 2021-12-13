fd93f714ea4e69914ab17332b45fe6d8b38fc4be
mbittu00 <mbittu000@gmail.com>
2021-12-13 13:39:07 +0530

    again fixed

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express=require('express')
 const mongo=require('./db')
 const app=express()
-let port=prosess.env.PORT || 4001
+let port=process.env.PORT || 4001
 app.use(express.json())
 mongo()
 app.use('/set',require('./route/set'))
 app.use('/get',require('./route/get'))
 app.use('/rm',require('./route/up'))
 
-app.listen(4001)
+app.listen(port)
